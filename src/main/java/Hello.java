b660ad537acc49c41363b16f71ed96a2b9fe69a2
samyside <samyside@yandex.ru>
2020-01-27 03:20:58 +0300

    Initial commit. helloworld

---
+++ src/main/java/Hello.java
@@ -0,0 +1,10 @@
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class Hello extends HttpServlet {
+    public void doGet(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws IOException {
+        httpServletResponse.getWriter().print("Hello from servlet");
+    }
+}
